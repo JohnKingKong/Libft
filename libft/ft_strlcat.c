1e8ec67ff0dde60b89bd0215305b11b4362a0870
jeanre <jeanre002@gmail.com>
2021-09-17 00:19:44 -0400

    from home

---
+++ libft/ft_strlcat.c
@@ -0,0 +1,42 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strlcat.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jvigneau <jvigneau@student.42quebec.c      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/09/16 21:52:52 by jvigneau          #+#    #+#             */
+/*   Updated: 2021/09/17 00:11:22 by jvigneau         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+// ecrire une fct qui reproduit a lidentique la fct strlcpy
+
+#include <stddef.h>
+
+size_t	ft_strlcat(char *dest, const char *src, size_t size);
+unsigned long	ft_strlen(const char *s);
+
+size_t	ft_strlcat(char *dest, const char *src, size_t size)
+{
+	size_t	i;
+	size_t	j;
+	size_t	k;
+	size_t	l;
+
+
+	i = 0;
+	j = 0;
+	while(dest[j])
+		j++;
+	k = ft_strlen(dest);
+	l = ft_strlen(src);
+	while (src[i] && ((k + i) + 1) < size)
+	{
+		dest[j] = src[i];
+		j++;
+		i++;
+	}
+	dest[j] = '\0';
+	return (k + l);
+}
--- libft/main.c
+++ libft/main.c
@@ -6,13 +6,13 @@
 /*   By: jvigneau <jvigneau@student.42quebec>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/09/14 12:57:46 by jvigneau          #+#    #+#             */
-/*   Updated: 2021/09/16 15:53:09 by jvigneau         ###   ########.fr       */
+/*   Updated: 2021/09/17 00:17:56 by jvigneau         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdio.h>
 #include <ctype.h>
-#include <string.h>
+#include <bsd/string.h>
 #include <stddef.h>
 
 int             ft_isalpha(int c);
@@ -27,45 +27,51 @@ void            *ft_memcpy(void *restrict dst, const void *restrict src, size_t 
 void            *ft_memmove(void *dst, const void *src, size_t len);
 int				ft_toupper(int c);
 int				ft_tolower(int c);
+size_t			ft_strlcat(char *dest, const char *src, size_t size);
+
 
 int main(void)
 {
-    int a = 'a';
-	int d = 'A';
-    int b = '4';
-    size_t antho = 4;
-    int c = '\t';
-    char s[] = "123456789";
-    char x[] = "asdasdadas";
-    char q[] = "YOLO";
-    char p[] = "oh noi12217398112o937!!!";
-
-    printf("ft_isalpha : %d \n", ft_isalpha(a));
-    printf("isalpha : %d \n", isalpha(a));
-    printf("ft_isdigit : %d \n", ft_isdigit(b));
-    printf("isdigit : %d \n", isdigit(b));
-    printf("ft_isalnum : %d \n", ft_isalnum(c));
-    printf("isalnum : %d \n", isalnum(c));
-    printf("ft_isascii : %d \n", ft_isascii(c));
-    printf("isascii : %d \n", isascii(c));
-    printf("ft_isprint : %d \n", ft_isprint(a));
-    printf("isprint : %d \n", isprint(a));
-    printf("ft_strlen : %lu \n", ft_strlen(s));
-    printf("strlen : %lu \n", strlen(s));
-    printf("memset : %s \n", memset(s, a, antho));
-    printf("ft_memset : %s \n" , ft_memset(s, a, antho));
-    printf("ft_bzero : %s \n", x);
-    ft_bzero(x, antho);
-    printf("after : %s \n", x);
-    printf("bzero : %s \n", s);
-    bzero(s, antho);
-    printf("after : %s \n", s);
-    printf("ft_memcpy : %s \n", ft_memcpy(q, p, 4));
-    printf("memcpy : %s \n", memcpy (q, p , 4));
-    printf("ft_memmove : %s \n", ft_memmove(q, p, 4));
-    printf("memmove : %s \n", memmove(q, p, 4));
-	printf("ft_toupper : %d \n", ft_toupper(a));
-	printf("toupper : %d \n", toupper(a));
-	printf("ft_tolower : %d \n", ft_tolower(d));
-	printf("tolower : %d \n", tolower(d));
+  //  int a = 'a';
+  //  int d = 'A';
+  //  int b = '4';
+  //  size_t antho = 4;
+  //  int c = '\t';
+  //  char s[] = "123456789";
+  //  cthar x[] = "asdasdadas";
+  //  char q[] = "YOLO";
+  //  char p[] = "oh noi12217398112o937!!!";
+  
+  //  printf("ft_isalpha : %d \n", ft_isalpha(a));
+  //  printf("isalpha : %d \n", isalpha(a));
+  //  printf("ft_isdigit : %d \n", ft_isdigit(b));
+  //  printf("isdigit : %d \n", isdigit(b));
+  //  printf("ft_isalnum : %d \n", ft_isalnum(c));
+  //  printf("isalnum : %d \n", isalnum(c));
+  //  printf("ft_isascii : %d \n", ft_isascii(c));
+  //  printf("isascii : %d \n", isascii(c));
+  //  printf("ft_isprint : %d \n", ft_isprint(a));
+  //  printf("isprint : %d \n", isprint(a));
+  //  printf("ft_strlen : %lu \n", ft_strlen(s));
+  //  printf("strlen : %lu \n", strlen(s));
+  //  printf("memset : %s \n", memset(s, a, antho));
+  //  printf("ft_memset : %s \n" , ft_memset(s, a, antho));
+  //  printf("ft_bzero : %s \n", x);
+  //  ft_bzero(x, antho);
+  //  printf("after : %s \n", x);
+  //  printf("bzero : %s \n", s);
+  //  bzero(s, antho);
+  //  printf("after : %s \n", s);
+  //  printf("ft_memcpy : %s \n", ft_memcpy(q, p, 4));
+  //  printf("memcpy : %s \n", memcpy (q, p , 4));
+  //  printf("ft_memmove : %s \n", ft_memmove(q, p, 4));
+  //  printf("memmove : %s \n", memmove(q, p, 4));
+  //  printf("ft_toupper : %d \n", ft_toupper(a));
+  //  printf("toupper : %d \n", toupper(a));
+  //  printf("ft_tolower : %d \n", ft_tolower(d));
+  //  printf("tolower : %d \n", tolower(d))
+  //  printf("ft_strlcat : %lu \n", ft_strlcat(q, p, (sizeof(p) + sizeof(q))));
+  //  printf("%s \n", q);
+  //  printf("strlcat : %lu \n", strlcat(q, p, 10));
+  //  1printf("%s \n", q);
 }
